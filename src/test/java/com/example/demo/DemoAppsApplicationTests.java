a5a84774a487451331d7997ccfa33a21f639834a
pujaric <chandu.java3@gmail.com>
2023-05-15 11:43:56 +0530

    Update DemoAppsApplicationTests.java

--- src/test/java/com/example/demo/DemoAppsApplicationTests.java
+++ src/test/java/com/example/demo/DemoAppsApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mockStatic;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.SpringApplication;
+
+@ExtendWith(MockitoExtension.class)
 class DemoAppsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void mainTest() {
+		DemoAppsApplication ulsFileServerApplication = new DemoAppsApplication();
+		mockStatic(SpringApplication.class);
+		DemoAppsApplication.main(new String[0]);
+		SpringApplication.run(DemoAppsApplication.class, new String[0]);
+		assertTrue(true);
 	}
 
 }
