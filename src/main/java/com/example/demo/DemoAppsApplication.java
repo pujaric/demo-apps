ba9038dc9924ff5d746bd60c107941dfd0da6461
pujaric <chandu.java3@gmail.com>
2024-01-24 20:39:43 +0530

    actuator endpoints added

--- src/main/java/com/example/demo/DemoAppsApplication.java
+++ src/main/java/com/example/demo/DemoAppsApplication.java
@@ -3,10 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.classic.Logger;
+import lombok.extern.log4j.Log4j;
+
 @SpringBootApplication
+@Log4j
 public class DemoAppsApplication {
 
 	public static void main(String[] args) {
+//		logger.info("");
 		SpringApplication.run(DemoAppsApplication.class, args);
 	}
 
